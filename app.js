6bca8e95f74bbf89e960ecdafbabc475d8c36057
WLpub <piciciwang@163.com>
2015-07-21 14:18:17 +0800

    submit

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.get('/xueqius', xueqiu.findAll);
 app.get('/xueqius' +'/:id', xueqiu.find);
 
 app.get('/list',function(req,res){
+	console.log(req.query);
 	res.send("{\"currentPage\": 2,\"pageSize\": 12,\"total\": 45,\"predicate\": \"comprehensive\",\"items\": [	{\"pid\" : 1,\"thumbnail\": \"http://img11.360buyimg.com/n7/jfs/t277/193/1005339798/768456/29136988/542d0798N19d42ce3.jpg\",\"logo\": \"1号店\",\"name\": \"苹果Apple 【1号海购】港版 iPhone 6 plus 4G手机 5.5英寸 A1524 GSM/WCDMA/TD-LTE(金色)\",\"price\": 5469.00,\"source\": \"第三方\",\"sales\" : 400,\"directPost\": \"是\",\"storage\": 200,\"updateTime\": \"2小时\",\"comments\" : 1,\"comprehensive\":12}]}");
 });
 app.get('/app',function(req,res){
