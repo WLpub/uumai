4907c8305fc34cfb9d633ffd1ed98a5b0aa8c06f
kanxg <xingang.kan@gmail.com>
2016-11-01 13:17:45 +0800

    add search history

--- public/scripts/controllers/search.js
+++ public/scripts/controllers/search.js
@@ -5,7 +5,7 @@
 		.module('uumaiApp')
 		.controller('SearchCtrl', SearchCtrl);
 
-	function SearchCtrl($rootScope, $scope, $location){
+	function SearchCtrl($rootScope, $scope, $window,$location){
 
 		$scope.goToList = function(){
 			if(angular.isUndefined($scope.searchWords)){
@@ -17,6 +17,21 @@
 		$scope.enterSearchWord = function(event){
 			if(event.keyCode !== 13) return;
 			$scope.goToList(); 
+		};
+		$scope.goToHistory = function(){
+			if(angular.isUndefined($scope.searchprdidurl)){
+				$scope.searchprdidurl = " ";
+			}
+			if($scope.searchprdidurl.startsWith("https")){
+				$scope.searchprdidurl="http"+$scope.searchprdidurl.substring(5,$scope.searchprdidurl.length);
+			}
+ 			$rootScope.searchprdidurl = $scope.searchprdidurl;
+			//alert($scope.searchprdidurl);
+ 			$window.open("/chromeextservice?traceurl="+$scope.searchprdidurl, '_blank');
+		};
+		$scope.enterSearchHistory = function(event){
+			if(event.keyCode !== 13) return;
+			$scope.goToHistory(); 
 		}
 	}
 	
